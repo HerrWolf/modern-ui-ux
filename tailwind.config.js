847013ba99baa01180a4f2e28b8d66c111539926
HerrWolf <webmasterp2m@gmail.com>
2024-11-11 14:24:52 -0800

    pricing finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require("tailwindcss/plugin");
+
+const rotateY = plugin(function ({ addUtilities }) {
+  addUtilities({
+    '.rotate-y-180': {
+      transform: 'rotateY(180deg)',
+    },
+    '.-rotate-y-180': {
+      transform: 'rotateY(-180deg)',
+    },
+  })
+})
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
@@ -21,5 +35,7 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    rotateY,
+  ],
 }
